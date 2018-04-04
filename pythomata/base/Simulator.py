d482dcd5d99c44e2dd50bd6e13ec03fc79dbb4a7
Marco Favorito <marco.favorito@gmail.com>
2018-04-04 19:01:40 +0200

    fix in Simulator state id usage

--- pythomata/base/Simulator.py
+++ pythomata/base/Simulator.py
@@ -14,11 +14,13 @@ class Simulator(object):
 
     def make_transition(self, s:Symbol):
         assert s in self.dfa.alphabet.symbols
-        transitions = self.dfa.transition_function[self.cur_state]
+        transitions = self.dfa.transition_function[self.id2state[self.cur_state]]
+
         self.cur_state = self.state2id[transitions[s]]
 
+
     def is_true(self):
-        return self.state2id[self.cur_state] in self.dfa.accepting_states
+        return self.id2state[self.cur_state] in self.dfa.accepting_states
 
     def word_acceptance(self, word: List[Symbol]):
         self.reset()
@@ -27,4 +29,4 @@ class Simulator(object):
         return self.is_true()
 
     def reset(self):
-        self.cur_state = self.dfa.initial_state
+        self.cur_state = self.state2id[self.dfa.initial_state]
