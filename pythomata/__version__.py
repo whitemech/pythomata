f18352b439bac666cdfb5e63aea2d2519b78716c
MarcoFavorito <marco.favorito@gmail.com>
2019-04-07 14:05:41 +0200

    update version number to 0.1.7rc0

--- pythomata/__version__.py
+++ pythomata/__version__.py
@@ -3,8 +3,8 @@
 __title__ = "pythomata"
 __description__ = "A Python package implementing automata."
 __url__ = "https://github.com/marcofavorito/pythomata.git"
-__version__ = "0.1.6"
-__build__ = 0x000160
+__version__ = "0.1.7rc0 "
+__build__ = 0x000170
 __author__ = "Marco Favorito"
 __author_email__ = "marco.favorito@gmail.com"
 __license__ = "MIT license"
