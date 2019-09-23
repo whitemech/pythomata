d100413b67c7e17dfa3370ef7e22a8075a37cc12
MarcoFavorito <marco.favorito@gmail.com>
2019-09-23 16:25:16 +0200

    bump version to 1.1.1

--- pythomata/__version__.py
+++ pythomata/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "pythomata"
 __description__ = "A Python package implementing automata."
 __url__ = "https://github.com/marcofavorito/pythomata.git"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "Marco Favorito"
 __author_email__ = "marco.favorito@gmail.com"
 __license__ = "Apache License"
