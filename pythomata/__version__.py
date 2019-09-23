8b3683f28c46fe1ba56259b71899c640fbd69bc5
MarcoFavorito <marco.favorito@gmail.com>
2019-09-23 13:10:34 +0200

    update license.
    from MIT to Apache 2.0

--- pythomata/__version__.py
+++ pythomata/__version__.py
@@ -3,10 +3,9 @@
 __title__ = "pythomata"
 __description__ = "A Python package implementing automata."
 __url__ = "https://github.com/marcofavorito/pythomata.git"
-__version__ = "1.0.0a0"
-__build__ = 0x000170
+__version__ = "1.1.0"
 __author__ = "Marco Favorito"
 __author_email__ = "marco.favorito@gmail.com"
-__license__ = "MIT license"
-__copyright__ = "2019 Marco Favorito"
+__license__ = "Apache License"
+__copyright__ = "2018-2019 Marco Favorito"
 
